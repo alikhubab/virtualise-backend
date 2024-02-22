65aaa3ff0ddd70ee0843ccccfff788130c3c05ec
Khubaib Ali <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2024-02-23 01:03:46 +0500

    add try catch to firebase init

--- startup/firebase.js
+++ startup/firebase.js
@@ -2,8 +2,13 @@ const admin = require("firebase-admin")
 const serviceAccountKey = require("../inference-service-account-key.json")
 
 module.exports = function() {
+  try {
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccountKey, "newApp"),
   });
+  }catch (e) {
+    console.log(e)
+  }
 }
 
