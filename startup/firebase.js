681954401a366270101df8e4de9298329c78f37e
Khubaib Ali <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2024-02-23 01:00:57 +0500

    xxx

--- startup/firebase.js
+++ startup/firebase.js
@@ -2,9 +2,8 @@ const admin = require("firebase-admin")
 const serviceAccountKey = require("../inference-service-account-key.json")
 
 module.exports = function() {
-
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountKey),
+    credential: admin.credential.cert(serviceAccountKey, "newApp"),
   });
-
 }
+
